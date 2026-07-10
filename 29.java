70843ad6775c97c8f0857b23b97107d92fb58e87
ShahistaSamrin <shaikshahistasamrin@gmail.com>
2026-07-10 20:12:34 +0530

    Add files via upload

---
+++ 29.java
@@ -0,0 +1,34 @@
+class Solution {
+    public int divide(int dividend, int divisor) {
+
+        int INT_MAX = Integer.MAX_VALUE;
+        int INT_MIN = Integer.MIN_VALUE;
+
+        // Overflow case
+        if (dividend == INT_MIN && divisor == -1)
+            return INT_MAX;
+
+        // Determine sign
+        boolean negative = (dividend < 0) ^ (divisor < 0);
+
+        long dvd = Math.abs((long) dividend);
+        long dvs = Math.abs((long) divisor);
+
+        int quotient = 0;
+
+        while (dvd >= dvs) {
+            long temp = dvs;
+            int multiple = 1;
+
+            while (dvd >= (temp << 1)) {
+                temp <<= 1;
+                multiple <<= 1;
+            }
+
+            dvd -= temp;
+            quotient += multiple;
+        }
+
+        return negative ? -quotient : quotient;
+    }
+}
